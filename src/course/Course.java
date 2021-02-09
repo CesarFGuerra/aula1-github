6c95a3d32329fe92c0910f35234bdaa9c5a91075
CesarFGuerra <cesarfrancaguerra@hotmail.com>
2021-02-09 17:33:47 -0300

    acrescentada mensagem bom dia

--- src/course/Course.java
+++ src/course/Course.java
@@ -15,6 +15,7 @@ public class Course {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Bom dia");
         // TODO code application logic here
     }
     
